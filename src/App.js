67b3ed792b5e6013f9cd5c12f1fe4f21b61be4a9
Therakar <matteo.pau200@gmail.com>
2024-01-22 12:11:30 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+  //state
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("EUR");
+
   return (
     <div className="box">
       <h2>Currency Converter</h2>
       <div className="inputbox">
-        <input type="text" />
-        <select>
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+        <select
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        >
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
-        <select>
+        <select
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
